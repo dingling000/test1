e7f775901d00290dd5fbe9b0692a5dbc4a5a6a21
dingling000 <2020302111333@whu.edu.cn>
2025-04-11 13:49:12 +0800

    "xiugai"

--- src/main/java/com/yf/exam/ExamApplication.java
+++ src/main/java/com/yf/exam/ExamApplication.java
@@ -33,7 +33,7 @@ public class ExamApplication implements WebMvcConfigurer {
 		String ip = InetAddress.getLocalHost().getHostAddress();
 		String port = env.getProperty("server.port");
 		String path = env.getProperty("server.servlet.context-path");
-		String test;
+
 
 
 
